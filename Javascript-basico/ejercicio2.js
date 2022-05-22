66da90c7a12705f088db6ab7d18be4a1b50dfe4d
tecnopulsar <tecnopulsar@gmail.com>
2022-05-21 22:41:20 -0300

    ejercicio2

---
+++ Javascript-basico/ejercicio2.js
@@ -0,0 +1,10 @@
+var nombre="Juan Manuel";
+var edad = "46";
+let desarrollador=true;
+var fechaNacimiento= "01/02/1976"
+var Objet = {
+  titulo: "La economia familia",
+  autor: "Don tito",
+  fecha:"12.12.2021",
+  url:"http://www.cuspide.net"
+}
