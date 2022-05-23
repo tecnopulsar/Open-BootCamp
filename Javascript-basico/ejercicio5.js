edc6c5d8a6bec852a2e4afb7aff1de32f812ea48
tecnopulsar <tecnopulsar@gmail.com>
2022-05-23 17:32:28 -0300

    Ejercicio 5

---
+++ Javascript-basico/ejercicio5.js
@@ -0,0 +1,7 @@
+let altura_cm= 178
+let altura_m=1.78
+let peso_kg= 78.4
+let alturaRound =Math.round(altura_m)
+let pesoFloor =Math.floor(peso_kg)
+let booleano = (Number.MAX_VALUE+1 === Number.MAX_VALUE) 
+console.log(booleano);
