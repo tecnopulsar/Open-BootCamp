ee75d9f3590dd27f3cfdbafda7c9558f4aaf5309
tecnopulsar <tecnopulsar@gmail.com>
2022-05-23 17:08:27 -0300

    ejercicio4

---
+++ Javascript-basico/ejercicio4.js
@@ -0,0 +1,12 @@
+let nombre="Juan  Manuel"
+let apellido="Burdet"
+let estudiante= `${nombre}   ${apellido}`
+let estudianteMayus=estudiante.toUpperCase();
+let estudianteMinus=estudiante.toLowerCase();
+let lengthEstudiante= estudiante.length
+let primerCharEstufiante = estudiante.charAt(0)
+let ultimoCharEstufiante = estudiante.charAt(lengthEstudiante-1)
+let estudianteSinEspacios= estudiante.replace(/\s/g,'')
+let includeNombreEstudiante=estudiante.includes(nombre)
+
+
